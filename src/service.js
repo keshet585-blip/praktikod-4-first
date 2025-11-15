f304aa85a53e2629c7f65fe9bb636f99656a2d7b
Noa Keshet <keshet585@gmail.com>
2025-11-16 01:18:50 +0200

    my commit message

--- src/service.js
+++ src/service.js
@@ -1,26 +1,40 @@
 import axios from 'axios';
 
-const apiUrl = "https://localhost:7271"
+const apiUrl = "http://localhost:5260";
+
+// הגדרת כתובת ה-API כברירת מחדל
+axios.defaults.baseURL = apiUrl;
+
+// הוספת interceptor לטיפול בשגיאות
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error('API request error:', error);
+    return Promise.reject(error);
+  }
+);
 
 export default {
   getTasks: async () => {
-    const result = await axios.get(`${apiUrl}/items`)    
+    const result = await axios.get('/items');
     return result.data;
   },
 
-  addTask: async(name)=>{
-    console.log('addTask', name)
-    //TODO
-    return {};
+  addTask: async (name) => {
+    console.log('addTask', name);
+    const response = await axios.post('/items', { name });
+    return response.data;
   },
 
-  setCompleted: async(id, isComplete)=>{
-    console.log('setCompleted', {id, isComplete})
-    //TODO
-    return {};
+  setCompleted: async (id, isComplete) => {
+    console.log('setCompleted', { id, isComplete });
+    const response = await axios.put(`/items/${id}`, { isComplete });
+    return response.data;
   },
 
-  deleteTask:async()=>{
-    console.log('deleteTask')
+  deleteTask: async (id) => {
+    console.log('deleteTask', id);
+    await axios.delete(`/items/${id}`);
+    return {};
   }
 };
